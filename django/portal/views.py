08598d5361fedc9f4e2965ecd604d2a4f40f275e
Kunal <gargkunal02@gmail.com>
2021-12-09 17:37:25 +0530

    Done all changes

--- django/portal/views.py
+++ django/portal/views.py
@@ -19,26 +19,22 @@ from .decorators import patient_required
 
 def login_request(request):
     if request.method == 'POST':
-        form = AuthenticationForm(request=request, data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                if request.user.is_doctor:
-                    messages.info(request, f"You are now logged in as {username}")
-                    return redirect('portal:doctor_home')
-                else:
-                    return redirect('portal:patient_home')
+        # form = AuthenticationForm(request=request, data=request.POST)
+        # print(request.POST)
+        username = request.POST['username']
+        password = request.POST['pass']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            if request.user.is_doctor:
+                messages.info(request, f"You are now logged in as {username}")
+                return redirect('portal:doctor_home')
             else:
-                messages.error(request, "Invalid username or password.")
+                return redirect('portal:patient_home')
         else:
             messages.error(request, "Invalid username or password.")
-    form = AuthenticationForm()
     return render(request = request,
-                    template_name = "registration/login.html",
-                    context={"form":form})
+                    template_name = "registration/login.html",)
 
 class SignUpView(TemplateView):
     template_name = 'portal/index-home.html'
